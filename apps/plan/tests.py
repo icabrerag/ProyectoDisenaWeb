9f644f26cd037365793c582b53cd645b344d0c93
Ignacio <icabrera.g17@gmail.com>
2019-12-16 17:54:24 -0300

    caso de prueba

--- apps/plan/tests.py
+++ apps/plan/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class mis_tests (unittest.TestCase):
+
+
+    def plan_list(request, pk):
+        plan = get_object_or_404(plan, pk=pk)
+        return render(request, 'plan_list.html', {'plan': plan })
